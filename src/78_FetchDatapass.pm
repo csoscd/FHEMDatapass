4246d40daf586d4600e92f137f45a43576a48143
csoscd <christian.schulz@sabine-und-christian.de>
2018-09-12 21:19:05 +0200

    First version which requests and parses data

--- src/78_FetchDatapass.pm
+++ src/78_FetchDatapass.pm
@@ -83,7 +83,7 @@ sub FetchDatapass_Define($$) {
     my $interval = 60;
 
     $hash->{NAME} = $name;
-
+    $hash->{URL} = "http://datapass.de/";
     $hash->{STATE}    = "Initializing" if $interval > 0;
     $hash->{helper}{INTERVAL} = $interval;
     $hash->{MODEL}    = $type;
@@ -93,7 +93,7 @@ sub FetchDatapass_Define($$) {
   
   # Starting the timer to get data from www.datapass.de.
   
-  # InternalTimer(gettimeofday() + 10, "FetchDatapass_GetData", $hash, 0);
+  InternalTimer(gettimeofday() + 10, "FetchDatapass_GetData", $hash, 0);
 
   $hash->{fhem}{modulVersion} = '$V0.0.1$';
  
@@ -102,6 +102,110 @@ sub FetchDatapass_Define($$) {
 #
 # end FetchDatapass_Define
 ###############################################
+
+###############################################
+# begin FetchDatapass_GetData
+#
+sub FetchDatapass_GetData($) {
+
+	my ($hash) = @_;
+	my $name = $hash->{NAME};
+	my $interval = FetchDatapass_getInterval($hash);
+
+	FetchDatapass_Log($hash, 5, "$name: Getting data from base url from $hash->{URL}");
+
+	FetchDatapass_PerformHttpRequest($hash, $hash->{URL}, "DATA");
+
+	# Now add a next timer for getting the data
+	InternalTimer(gettimeofday() + $interval, "FetchDatapass_GetData", $hash, 0);
+}
+#
+# end FetchDatapass_GetData
+###############################################
+
+
+###############################################
+# begin FetchDatapass_PerformHttpRequest
+#
+#
+# Perform the http request as a non-blocking request
+#
+sub FetchDatapass_PerformHttpRequest($$)
+{
+    my ($hash, $url, $callname) = @_;
+    my $name = $hash->{NAME};
+
+    $hash->{STATE}    = "Receiving data";
+
+    my $param = {
+                    url        => $url,
+                    timeout    => 5,
+                    hash       => $hash,                                                                                 # Muss gesetzt werden, damit die Callback funktion wieder $hash hat
+                    method     => "GET",                                                                                 # Lesen von Inhalten
+                    header     => "User-Agent: Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36\r\nAccept: text/html",                            # Den Header gemäß abzufragender Daten ändern
+                    callback   => \&FetchDatapass_ParseHttpResponse,                                                    # Diese Funktion soll das Ergebnis dieser HTTP Anfrage bearbeiten
+                    call       => $callname
+                };
+
+    FetchDatapass_Log($hash, 5, "$name: Executing non-blocking get for $url");
+
+    HttpUtils_NonblockingGet($param);                                                                                    # Starten der HTTP Abfrage. Es gibt keinen Return-Code. 
+}
+#
+# end FetchDatapass_PerformHttpRequest
+###############################################
+
+###############################################
+# begin FetchDatapass_ParseHttpResponse
+#
+sub FetchDatapass_ParseHttpResponse($)
+{
+    my ($param, $err, $data) = @_;
+    my $hash = $param->{hash};
+    my $name = $hash->{NAME};
+    my $interval = FetchDatapass_getInterval($hash);
+    
+    $hash->{STATE}    = "Data received";
+
+    if($err ne "")                                                                                                      # wenn ein Fehler bei der HTTP Abfrage aufgetreten ist
+    {
+	$hash->{STATE}    = "Connection error";
+        FetchDatapass_Log($hash, 1, "error while requesting ".$param->{url}." - $err");                                            # Eintrag fürs Log
+	if ($param->{call} eq "DATA") {
+	  #
+	  # if DATA Call failed, try again in 60 seconds
+	  #
+	  # InternalTimer(gettimeofday() + 60, "FetchDatapass_GetData", $hash, 0);
+	  $hash->{STATE}    = "Connection error getting data";
+	  FetchDatapass_Log($hash, 1, "DATA call to www.datapass.de failed");                                                         # Eintrag fürs Log
+	} else {
+	  FetchDatapass_Log($hash, 1, "Call to www.datapass.de failed for ".$param->{call}. "(".$param->{url}.")");                                                         # Eintrag fürs Log
+	}
+    }
+    elsif($data ne "")                                                                                                  # wenn die Abfrage erfolgreich war ($data enthält die Ergebnisdaten des HTTP Aufrufes)
+    {
+        FetchDatapass_Log($hash, 5, "url ".$param->{url}." returned: $data");                                                         # Eintrag fürs Log
+
+	if ($param->{call} eq "DATA") {
+		#
+		# This is the standard data call
+		# 
+		#FetchDatapass_GetData_Parse($hash, $data, $param->{call});
+		my ($decimal, $fraction, $unita, $total, $totalunit) = $data =~ m/<div class="barTextBelow.*"><span class="colored">(\d*),?(\d*).*([a-zA-Z]{2})<\/span> von (\d*).*([a-zA-Z]{2}) verbraucht<\/div>/;
+		FetchDatapass_Log($hash, 5, "Ergebnis: ".$decimal.",".$fraction.$unita."/".$total.$totalunit);
+	} else {
+		FetchDatapass_Log($hash, 1, "Error. Unknown call for ".$param->{call}); 
+	}
+    }
+    
+    # Damit ist die Abfrage zuende.
+    # Evtl. einen InternalTimer neu schedulen
+}
+#
+# end FetchDatapass_ParseHttpResponse
+###############################################
+
+
 
 ###############################################
 # begin FetchDatapass_Set
@@ -182,6 +286,51 @@ sub FetchDatapass_InitAttr($) {
 }
 #
 # end FetchDatapass_InitAttr
+###############################################
+
+#
+# Helper Functions
+#
+###############################################
+# begin FetchDatapass_getInterval
+#
+#
+# Helper function to the a valid interval value for data requests
+#
+sub FetchDatapass_getInterval($) {
+	my ($hash) = @_;
+	my $name = $hash->{NAME};
+	my $is_day = isday();
+
+	my $interval = $attr{$name}{interval};
+	# if there is no interval given, use the internal default
+	if ($interval eq "") {
+		# use default interval if none is given
+		$interval = $hash->{helper}{INTERVAL};
+	}
+	
+	# check if sun has gone. If yes and a night interval is set, use the night interval
+	if ($is_day eq "0") {
+		my $interval_night = $attr{$name}{interval_night};
+		if ($interval_night ne "") {
+			$interval = $interval_night;				
+		}
+	}
+
+	# if interval is less then 5, we will use ten seconds as minimum
+	if ($interval < 5) {
+		# the minimum value
+		$interval = 10;
+		$attr{$name}{interval} = 10;
+	}
+	
+	$hash->{helper}{last_used_interval} = $interval;
+	$hash->{helper}{last_is_day} = $is_day;
+	
+	return $interval;
+}
+#
+# end FetchDatapass_getInterval
 ###############################################
 
 
