9c6a1c1a6ebe91f4e3592617c8300d6ec2f4d845
csoscd <christian.schulz@sabine-und-christian.de>
2018-09-12 20:11:07 +0200

    Added Initialize and Define

--- src/78_FetchDatapass.pm
+++ src/78_FetchDatapass.pm
@@ -36,3 +36,69 @@ sub FetchDatapass_Log($$$)
 #
 # end FetchDatapass_Log
 ###############################################
+
+###############################################
+# begin FetchDatapass_Initialize
+#
+sub FetchDatapass_Initialize($) {
+
+    my ($hash) = @_;
+    my $TYPE = "FetchDatapass";
+
+    $hash->{DefFn}    = $TYPE . "_Define";
+    $hash->{UndefFn}  = $TYPE . "_Undefine";
+    $hash->{SetFn}    = $TYPE . "_Set";
+    $hash->{GetFn}    = $TYPE . "_Get";
+    $hash->{NotifyFn} = $TYPE . "_Notify";
+
+    $hash->{NOTIFYDEV} = "global";
+
+    $hash->{DbLog_splitFn}= $TYPE . "_DbLog_splitFn";
+#    $hash->{AttrFn}       = $TYPE . "_Attr";
+
+
+ $hash->{AttrList} = ""
+    . "disable:1,0 "
+    . "interval "
+    . "interval_night "
+    . $readingFnAttributes
+  ;
+}
+#
+# end FetchDatapass_Initialize
+###############################################
+
+###############################################
+# begin FetchDatapass_Define
+#
+sub FetchDatapass_Define($$) {
+
+    my ($hash, $def) = @_;
+    my @args = split("[ \t][ \t]*", $def);
+
+    return "Usage: define <name> FetchDatapass" if(@args <1 || @args >2);
+
+    my $name = $args[0];
+    my $type = "FetchDatapass";
+    my $interval = 60;
+
+    $hash->{NAME} = $name;
+
+    $hash->{STATE}    = "Initializing" if $interval > 0;
+    $hash->{helper}{INTERVAL} = $interval;
+    $hash->{MODEL}    = $type;
+    
+  #Clear Everything, remove all timers for this module
+  RemoveInternalTimer($hash);
+  
+  # Starting the timer to get data from www.datapass.de.
+  
+  # InternalTimer(gettimeofday() + 10, "FetchDatapass_GetData", $hash, 0);
+
+  $hash->{fhem}{modulVersion} = '$V0.0.1$';
+ 
+  return undef;
+}
+#
+# end FetchDatapass_Define
+###############################################
